5a078b3fc9f15048f7d20948bd724d11ec08f3a8
ett5 <eti0548592441@gmail.com>
2024-12-23 17:06:58 +0200

    add IService

---
+++ MatchMaker.API/MatchMaker.Core/Services/IIdeaService.cs
@@ -0,0 +1,18 @@
+﻿using MatchMaker.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaker.Core.Services
+{
+    internal interface IIdeaService
+    {
+        Task<List<Idea>> GetListAsync();
+        Task<Idea?> GetByIdAsync(int id);
+        Idea Add(Idea entity);
+        Idea Update(Idea idea);
+        void Delete(Idea idea);
+    }
+}
