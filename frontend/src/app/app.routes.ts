87938a40271cb87a5cd5e1734294f7e6280e2205
yulifrank <148456398+yulifrank@users.noreply.github.com>
2024-12-24 17:04:33 +0200

    fix route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,17 @@
-import { Routes } from '@angular/router';
-import { provideRouter } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from '../components/home-page/home-page.component';
 import { PersonsComponent } from '../components/persons/persons.component';
 import { IdeasComponent } from '../components/ideas/ideas.component';
-// import { NewPersonComponent } from '../components/new-person/new-person.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'person', component: PersonsComponent },
   { path: 'ideas', component: IdeasComponent },
-//   { path: 'new-person', component: NewPersonComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
